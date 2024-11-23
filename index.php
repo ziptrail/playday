722cdbf5baef2346bae9a2d85d779092209ceca7
ziptrail <45323560+ziptrail@users.noreply.github.com>
2024-11-23 23:11:58 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+
+include "db_conn.php";
+include "utils.php";
+
+?>
+
+<html data-wf-domain="playday-a06ea0.webflow.io" data-wf-page="670c91daa4b9a20627e5f16d" data-wf-site="670c91daa4b9a20627e5f12a" data-wf-status="1" lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta content="Store - Webflow HTML website template" property="og:title"/>
+        <meta content="https://cdn.prod.website-files.com/5c6eefaaeddf9248ac13bc72/5cdc90c195678d12d5f23f33_Store%20SEO.jpg" property="og:image"/>
+        <meta content="Store - Webflow HTML website template" property="twitter:title"/>
+        <meta content="https://cdn.prod.website-files.com/5c6eefaaeddf9248ac13bc72/5cdc90c195678d12d5f23f33_Store%20SEO.jpg" property="twitter:image"/>
+        <meta content="width=device-width, initial-scale=1" name="viewport"/><meta content="Webflow" name="generator"/>
+        <title>Главная</title>
+        <link href="https://cdn.prod.website-files.com/670c91daa4b9a20627e5f12a/css/playday-a06ea0.webflow.844c01b93.css" rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com" rel="preconnect"/>
+        <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+        <script type="text/javascript">WebFont.load({  google: {    families: [
+            "Montserrat:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+            ]  }});</script>
+        <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";
+            n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+        <link href="https://cdn.prod.website-files.com/img/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
+        <link href="https://cdn.prod.website-files.com/img/webclip.png" rel="apple-touch-icon"/>
+    </head>
+    <body>
+        <div data-collapse="medium" data-animation="default" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navigation w-nav">
+            <div class="navigation-items">
+                <div class="navigation-wrap">
+                    <nav role="navigation" class="navigation-items w-nav-menu">
+                        <a href="/PlayDayFrontend/index.php" aria-current="page" class="navigation-item w-nav-link w--current">Главная</a>
+                        <a href="/PlayDayFrontend/catalog.php" class="navigation-item w-nav-link">Каталог</a>
+                        <a href="/PlayDayFrontend/delivery.php" class="navigation-item w-nav-link">Доставка</a>
+                        <a href="/PlayDayFrontend/contact.php" class="navigation-item w-nav-link">Контакты</a>
+                        <a href="/PlayDayFrontend/reviews.php" class="navigation-item w-nav-link">Отзывы</a>
+                    </nav>
+                    <div class="menu-button w-nav-button">
+                        <img src="https://cdn.prod.website-files.com/670c91daa4b9a20627e5f12a/670c91daa4b9a20627e5f1af_menu-icon.png" width="22" alt="" class="menu-icon"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section cc-store-home-wrap">
+            <div class="intro-header">
+                <div class="intro-content">
+                    <div class="intro-text">
+                        <div class="heading-jumbo">PlayDay<br/></div>
+                        <div class="paragraph-bigger cc-bigger-light">Cамый крупный магазин видеоигр в <strong>Барнауле</strong></div>
+                    </div>
+                </div>
+            </div>
+            <div class="container2">
+                <div class="main-heading-wrap">
+                    <h1><em>Добро пожаловать в самый крупный магазин видеоигр в Барнауле!</em></h1>
+                    <div class="paragraph-bigger cc-bigger-light"><strong>У нас вы найдёте:</strong><br/></div>
+                        <ul role="list">
+                            <li>Огромный выбор игр для консолей PlayStation, Xbox, Nintendo и многих других платформ!</li>
+                            <li>Коллекционные фигурки, брелки, значки и другой гиковский стафф!</li>
+                            <li>Новые и поддержанные консоли PS, Xbox, Nintendo, а так же возможность обменять свою старую приставку на нашу по системе «Trade-in»!</li>
+                        </ul>
+                </div>
+            </div>
+        </div>
+        <div class="section">
+            <div class="container2">
+                <div class="w-layout-grid footer">
+                    <div id="w-node-_88a386dd-8f07-0c34-70f0-2d9f87e2971d-87e29718" class="label">Сайт магазина PlayDay</div>
+                    <div id="w-node-_88a386dd-8f07-0c34-70f0-2d9f87e2971f-87e29718" class="links-section-footer">
+                        <a href="https://vk.com/playday22" aria-current="page" class="footer-link w--current">VK</a>
+                        <a href="https://t.me/PlayDay22Barnaul" class="footer-link">Telegram</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script 
+            src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=670c91daa4b9a20627e5f12a" 
+            type="text/javascript" 
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" 
+            crossorigin="anonymous"></script>
+    </body>
+</html>
